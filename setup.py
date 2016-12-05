d53e8567d0188aa8b6f76b253cf9920c8fe535bf
Joerg Rings <mail@rings.de>
2016-12-04 19:34:43 -0600

    Enable setup.py develop

--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@
 
 from distutils.core import setup
 
+import setuptools
+
+setuptools  # Just for my editor
+
 setup(name="Folivora",
       version="0.0.1",
       description="Tools to save energy with CT50 radio thermostats",
