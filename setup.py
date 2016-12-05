87ccc7449319c27914a5e3d4cfef07a11ad7a88f
Joerg Rings <mail@rings.de>
2016-12-04 19:31:57 -0600

    Added a simple setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="Folivora",
+      version="0.0.1",
+      description="Tools to save energy with CT50 radio thermostats",
+      author="Joerg Rings",
+      author_email="mail@joergrings.com",
+      url="https://github.com/jrings/folivora",
+      packages=["folivora", "web"],
+      )
